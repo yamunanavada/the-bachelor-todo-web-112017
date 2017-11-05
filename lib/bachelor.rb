4f1034b895282ba14880c990113df8d182d4d163
Yamuna Navada <yamunanavada@gmail.com>
2017-11-05 18:12:12 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,14 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |name|
+    name.each do |stats, val|
+      if stats == "status"
+        if val == "Winner"
+          return name["name"]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
